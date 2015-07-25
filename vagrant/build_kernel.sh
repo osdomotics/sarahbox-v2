ffac200327b7e13cc497e493ad45e943e5ceddbb
Florian Schweikert <kelvan@ist-total.org>
2015-07-26 00:35:24 +0200

    Rename kernel config

--- vagrant/build_kernel.sh
+++ vagrant/build_kernel.sh
@@ -2,7 +2,7 @@
 
 wget -c https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.1.1.tar.xz
 tar -xf linux-4.1.1.tar.xz
-cp /vagrant/kernel-config-4.1 linux-4.1.1/.config
+cp /vagrant/kernel-config-4.1.1 linux-4.1.1/.config
 cd linux-4.1.1
 
 make ARCH=arm zImage dtbs
