cd776eb831f9302d743ebcfdb1de4a737d14887a
Florian Schweikert <kelvan@ist-total.org>
2015-07-25 21:52:37 +0200

    Update to Linux 4.1.1 and uboot 2015-07

--- vagrant/build_kernel.sh
+++ vagrant/build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget -c https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.1.tar.xz
-tar -xf linux-4.1.tar.xz
-cp /vagrant/kernel-config-4.1 linux-4.1/.config
-cd linux-4.1
+wget -c https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.1.1.tar.xz
+tar -xf linux-4.1.1.tar.xz
+cp /vagrant/kernel-config-4.1 linux-4.1.1/.config
+cd linux-4.1.1
 
 make ARCH=arm zImage dtbs
