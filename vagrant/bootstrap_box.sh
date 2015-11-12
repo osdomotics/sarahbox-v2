0096166a85692593a270e611bb9a83549a6082a8
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2015-11-12 16:17:43 +0100

    updated box bootstrap, switched to http://sarahbox.osdomotics.com

--- vagrant/bootstrap_box.sh
+++ vagrant/bootstrap_box.sh
@@ -9,4 +9,18 @@ apt-get update
 # base tools :)
 apt-get install -y vim
 # build tools
-apt-get install -y binfmt-support debootstrap qemu-user-static crossbuild-essential-armhf kpartx u-boot-tools dosfstools device-tree-compiler
+apt-get install -y binfmt-support debootstrap qemu-user-static crossbuild-essential-armhf kpartx u-boot-tools dosfstools device-tree-compiler ncurses-dev zerofree
+
+#cleanup
+rm -f /var/cache/apt/*pkgcache.bin /var/cache/apt/archives/* /var/lib/apt/lists/*
+sed -i "s/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=1/" /etc/default/grub
+update-grub2
+#now boot the vm manually in recovery mode and run:
+#systemctl stop networking
+#mount -o remount,ro /
+#zerofree /dev/sda1
+#shutdown -h 0
+
+#adjust box memory and other settings in virtualbox
+#and export the box with
+#vagrant package
