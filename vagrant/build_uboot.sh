11199616c186679bf49f1fdbf2ae83e8405a8ef2
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2016-10-08 20:17:50 +0200

    u-boot speed optimizations
    bootdelay gives us 2 seconds
    usb probes take ~2 seconds with one device, even more if ie a hub is connected

--- vagrant/build_uboot.sh
+++ vagrant/build_uboot.sh
@@ -7,4 +7,10 @@ wget -c ftp://ftp.denx.de/pub/u-boot/u-boot-$UBOOTVER.tar.bz2
 tar -xf u-boot-$UBOOTVER.tar.bz2
 cd u-boot-$UBOOTVER
 make CROSS_COMPILE=arm-linux-gnueabihf- `cat /vagrant/$1/uboot.conf`
+# minimize boot delay
+sed -i "s/CONFIG_BOOTDELAY=[0-9][0-9]*/CONFIG_BOOTDELAY=0/" .config
+# disable usb ehci drivers to prevent probes
+sed -i "s/CONFIG_USB_HOST=y//" .config
+sed -i "s/CONFIG_USB_EHCI_HCD=y/# CONFIG_USB_EHCI_HCD is not set/" .config
+sed -i "s/CONFIG_USB_EHCI=y/# CONFIG_USB_EHCI is not set/" .config
 make CROSS_COMPILE=arm-linux-gnueabihf-
