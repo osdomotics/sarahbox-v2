69cb404a2b478f2de209db005a5849783f3f361d
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2016-10-09 17:23:36 +0200

    vagrantbox update provision script for current debian box
    needs virtualbox addons
    needs curl

--- vagrantbox/bootstrap_box.sh
+++ vagrantbox/bootstrap_box.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-#add embedian cross toolchain repo
+#enable contrib repo for virtualbox guest addons
+sed -i "s/deb http:\/\/httpredir.debian.org\/debian jessie main/deb http:\/\/httpredir.debian.org\/debian jessie main contrib/" /etc/apt/sources.list
+
+apt-get update
+# base tools :)
+apt-get install -y vim curl
+# virtualbox guest addons
+apt-get install -y virtualbox-guest-dkms
+
+# add embedian cross toolchain repo
 curl --silent http://emdebian.org/tools/debian/emdebian-toolchain-archive.key | apt-key add -
-#add following line to sources
 echo "deb http://emdebian.org/tools/debian/ jessie main" > /etc/apt/sources.list.d/emdebian.list
 dpkg --add-architecture armhf
 apt-get update
-# base tools :)
-apt-get install -y vim
+
 # build tools
 apt-get install -y binfmt-support debootstrap qemu-user-static crossbuild-essential-armhf kpartx u-boot-tools dosfstools device-tree-compiler ncurses-dev zerofree
 
