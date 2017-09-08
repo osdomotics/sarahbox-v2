6e68c16318c8e23b2599320f9e5af6fbe4b32932
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2017-09-08 11:02:08 +0200

    fail uboot build if config doesn't exist

--- vagrant/build_uboot.sh
+++ vagrant/build_uboot.sh
@@ -6,7 +6,8 @@ set -e
 wget -c ftp://ftp.denx.de/pub/u-boot/u-boot-$UBOOTVER.tar.bz2
 tar -xf u-boot-$UBOOTVER.tar.bz2
 cd u-boot-$UBOOTVER
-make CROSS_COMPILE=arm-linux-gnueabihf- `cat /vagrant/$1/uboot.conf`
+BOARDCONFIG=$(cat /vagrant/$1/uboot.conf)
+make CROSS_COMPILE=arm-linux-gnueabihf- $BOARDCONFIG
 # minimize boot delay
 sed -i "s/CONFIG_BOOTDELAY=[0-9][0-9]*/CONFIG_BOOTDELAY=0/" .config
 # disable usb ehci drivers to prevent probes
