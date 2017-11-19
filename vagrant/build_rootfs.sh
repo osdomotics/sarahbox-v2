6021fc30d0d01fd9e4e0b9ecfcdd6e317c5cd240
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2017-11-19 19:57:44 +0100

    actually export locales settings

--- vagrant/build_rootfs.sh
+++ vagrant/build_rootfs.sh
@@ -92,7 +92,7 @@ EOF
 fi
 
 #we have no locales, make apt and friends a bit more friendly
-echo "LC_ALL=C" >> /etc/profile
+echo "export LC_ALL=C" >> /etc/profile
 
 #clean up rootfs a bit
 rm -f armstretchchroot/var/cache/apt/archives/*.deb
