bdb0ce8e002a122aa1c6df0b25d8b0a5c812c0d4
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2019-09-15 12:41:10 +0200

    qemu-nbd creates nbd0*p devices nowadays

--- vagrantbox/finalize.sh
+++ vagrantbox/finalize.sh
@@ -8,11 +8,9 @@ echo disk image is "$DISK"
 echo mounting image via qmeu-nbd
 sudo modprobe nbd
 sudo qemu-nbd -c /dev/nbd0 "$DISK"
-sudo kpartx -avs /dev/nbd0
 
 echo optimizing image
 sudo zerofree -v /dev/mapper/nbd0p1
 
 echo unmounting image
-sudo kpartx -dvs /dev/nbd0
 sudo qemu-nbd -d /dev/nbd0
