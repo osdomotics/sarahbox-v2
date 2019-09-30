8ccfaf3acb12466adc95c8ea2eab29714efff924
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2019-09-30 23:32:28 +0200

    update u-boot to 2019.07

--- vagrant/build_uboot.sh
+++ vagrant/build_uboot.sh
@@ -3,7 +3,7 @@ set -e
 
 . /vagrant/vagrant/settings.sh
 
-wget -c ftp://ftp.denx.de/pub/u-boot/u-boot-$UBOOTVER.tar.bz2
+wget -c https://ftp.denx.de/pub/u-boot/u-boot-$UBOOTVER.tar.bz2
 tar -xf u-boot-$UBOOTVER.tar.bz2
 cd u-boot-$UBOOTVER
 BOARDCONFIG=$(cat /vagrant/$1/uboot.conf)
@@ -11,7 +11,9 @@ make CROSS_COMPILE=arm-linux-gnueabihf- $BOARDCONFIG
 # minimize boot delay
 sed -i "s/CONFIG_BOOTDELAY=[0-9][0-9]*/CONFIG_BOOTDELAY=0/" .config
 # disable usb ehci drivers to prevent probes
-sed -i "s/CONFIG_USB_HOST=y//" .config
+sed -i "s/CONFIG_USB_HOST=y/# CONFIG_USB_HOST is not set/" .config
 sed -i "s/CONFIG_USB_EHCI_HCD=y/# CONFIG_USB_EHCI_HCD is not set/" .config
-sed -i "s/CONFIG_USB_EHCI=y/# CONFIG_USB_EHCI is not set/" .config
+sed -i "s/CONFIG_USB_EHCI_GENERIC=y/# CONFIG_USB_EHCI_GENERIC is not set/" .config
+sed -i "s/CONFIG_USB_OHCI_HCD=y/# CONFIG_USB_OHCI_HCD is not set/" .config
+sed -i "s/CONFIG_USB_OHCI_GENERIC=y/# CONFIG_USB_OHCI_GENERIC is not set/" .config
 make CROSS_COMPILE=arm-linux-gnueabihf-
